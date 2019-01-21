84248e5816f269da08b6da6cf23cdb8b92306ebe
yuma140902 <yuma140902@gmail.com>
2019-01-21 20:48:42 +0900

    クワで耕したときにたまに金印をドロップするようにした

--- src/main/java/yuma140902/mcmod/ancienttombs/ModConfigCore.java
+++ src/main/java/yuma140902/mcmod/ancienttombs/ModConfigCore.java
@@ -5,7 +5,8 @@ import net.minecraftforge.common.config.Configuration;
 
 public class ModConfigCore {
 	public static final String
-		CATEGORY_GENERAL = "General";
+		CATEGORY_GENERAL = "General",
+		CATEGORY_DIFFICULTY = CATEGORY_GENERAL + ".Difficulty";
 	
 	public static final String
 		CONFIG_PROP_LANGKEY = "config.ancienttombs.prop.",
@@ -13,6 +14,7 @@ public class ModConfigCore {
 	
 	public static Configuration cfg;
 	
+	public static int goldenSealRarity;
 	
 	public static void loadConfig(FMLPreInitializationEvent event) {
 		cfg = new Configuration(event.getSuggestedConfigurationFile(), true);
@@ -22,12 +24,20 @@ public class ModConfigCore {
 	
 	private static void initConfig() {
 		// General
-		cfg.addCustomCategoryComment(CATEGORY_GENERAL, "Settings of UpToDateMod");
+		cfg.addCustomCategoryComment(CATEGORY_GENERAL, "古墳MODの設定");
 		cfg.setCategoryLanguageKey(CATEGORY_GENERAL, CONFIG_CATEGORY_LANGKEY + "general");
 		cfg.setCategoryRequiresMcRestart(CATEGORY_GENERAL, true);
+		
+		// Difficulty
+		cfg.addCustomCategoryComment(CATEGORY_DIFFICULTY, "難易度に関する設定");
+		cfg.setCategoryLanguageKey(CATEGORY_DIFFICULTY, CONFIG_CATEGORY_LANGKEY + "difficulty");
 	}
 	
 	public static void syncConfig() {
+		// Difficulty
+		goldenSealRarity = cfg.getInt("goldenSealRarity", CATEGORY_DIFFICULTY, 5000, 1, Integer.MAX_VALUE, 
+				"金印の珍しさです。大きいほど出にくいです。", 
+				CONFIG_PROP_LANGKEY + "golden_seal_rarity");
 		cfg.save();
 	}
 	
--- src/main/java/yuma140902/mcmod/ancienttombs/event_handler/CommonEventHandler.java
+++ src/main/java/yuma140902/mcmod/ancienttombs/event_handler/CommonEventHandler.java
@@ -1,14 +1,39 @@
 package yuma140902.mcmod.ancienttombs.event_handler;
 
+import java.util.Random;
 import cpw.mods.fml.client.event.ConfigChangedEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import net.minecraftforge.event.entity.player.UseHoeEvent;
 import yuma140902.mcmod.ancienttombs.ModAncientTombs;
 import yuma140902.mcmod.ancienttombs.ModConfigCore;
+import yuma140902.mcmod.ancienttombs.items.ModItems;
 
 public class CommonEventHandler {
 	private CommonEventHandler() {}
 	
 	public static final CommonEventHandler INSTANCE = new CommonEventHandler();
+	
+	@SubscribeEvent
+	public void onUseHoeEvent(UseHoeEvent event) {
+		int x = event.x;
+		int y = event.y;
+		int z = event.z;
+		World world = event.world;
+		EntityPlayer player = event.entityPlayer;
+		Random rand = event.world.rand;
+		
+		if(world.getBlock(x, y, z) != Blocks.farmland && rand.nextInt(ModConfigCore.goldenSealRarity) == 0) {
+			if(!world.isRemote) {
+				EntityItem goldenSeal = new EntityItem(world, x + 0.5D, y + 1.5D, z + 0.5D, new ItemStack(ModItems.goldenSeal));
+				world.spawnEntityInWorld(goldenSeal);
+			}
+		}
+	}
 	
 	@SubscribeEvent
 	public void onConfigChanged(ConfigChangedEvent.OnConfigChangedEvent event) {
