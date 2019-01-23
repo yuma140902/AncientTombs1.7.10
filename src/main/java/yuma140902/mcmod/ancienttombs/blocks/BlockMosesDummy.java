0ffa74c8c1923b3d5f3ca8f2d05eff5ec60e77b5
yuma140902 <yuma140902@gmail.com>
2019-01-23 19:15:56 +0900

    クリエイティブタブにモーセのダミーブロックを登録しないようにした

--- src/main/java/yuma140902/mcmod/ancienttombs/blocks/BlockMosesDummy.java
+++ src/main/java/yuma140902/mcmod/ancienttombs/blocks/BlockMosesDummy.java
@@ -1,9 +1,14 @@
 package yuma140902.mcmod.ancienttombs.blocks;
 
+import java.util.List;
 import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.BlockAir;
 import net.minecraft.block.ITileEntityProvider;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
+import net.minecraft.item.Item;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
@@ -19,12 +24,14 @@ public class BlockMosesDummy extends BlockAir implements ITileEntityProvider, IR
 	
 	@Override
 	public void register() {
-		this.setCreativeTab(ModAncientTombs.MOD_CREATIVE_TAB);
 		this.setBlockName(ModAncientTombs.MOD_ID + ".moses_dummy_block");
 		this.setBlockTextureName(ModAncientTombs.MOD_ID + ":moses_dummy_block");
 		GameRegistry.registerBlock(this, "moses_dummy_block");
 	}
 	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void getSubBlocks(Item item, CreativeTabs creativeTab, List list) {}
 	
 	@Override
 	public boolean canEntityDestroy(IBlockAccess world, int x, int y, int z, Entity entity) {
