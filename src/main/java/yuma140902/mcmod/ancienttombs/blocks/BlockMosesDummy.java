009dbed9f3a7426dd9d73bf5ec23035ab4411a7e
yuma140902 <yuma140902@gmail.com>
2019-01-23 19:14:03 +0900

    モーセのダミーブロックがBlockAirを継承するようになった
    これで引っかからなくなった

--- src/main/java/yuma140902/mcmod/ancienttombs/blocks/BlockMosesDummy.java
+++ src/main/java/yuma140902/mcmod/ancienttombs/blocks/BlockMosesDummy.java
@@ -1,18 +1,19 @@
 package yuma140902.mcmod.ancienttombs.blocks;
 
 import cpw.mods.fml.common.registry.GameRegistry;
-import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
+import net.minecraft.block.BlockAir;
+import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.entity.Entity;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 import yuma140902.mcmod.ancienttombs.IRegisterable;
 import yuma140902.mcmod.ancienttombs.ModAncientTombs;
+import yuma140902.mcmod.ancienttombs.tileentity.TileEntityMosesDummyBlock;
 
-public class BlockMosesDummy extends Block implements IRegisterable {
+public class BlockMosesDummy extends BlockAir implements ITileEntityProvider, IRegisterable {
 	public BlockMosesDummy() {
-		super(Material.air);
 		setResistance(-1);
-		setBlockBounds(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F);
 		setBlockUnbreakable();
 	}
 	
@@ -24,18 +25,14 @@ public class BlockMosesDummy extends Block implements IRegisterable {
 		GameRegistry.registerBlock(this, "moses_dummy_block");
 	}
 	
-	@Override
-	public boolean isOpaqueCube() {
-		return false;
-	}
-	
-	@Override
-	public boolean renderAsNormalBlock() {
-		return false;
-	}
 	
 	@Override
 	public boolean canEntityDestroy(IBlockAccess world, int x, int y, int z, Entity entity) {
 		return false;
+	}
+
+	@Override
+	public TileEntity createNewTileEntity(World world, int meta) {
+		return new TileEntityMosesDummyBlock();
 	}
 }
